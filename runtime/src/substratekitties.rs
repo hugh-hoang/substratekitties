19e6ec32dd0f0d252f7f34b9f92a83b6df61278b
Hugh Hoang <hugh.hoang@augensoftwaregroup.com>
2019-02-28 10:08:30 +1300

    feat: random hash

--- runtime/src/substratekitties.rs
+++ runtime/src/substratekitties.rs
@@ -1,12 +1,20 @@
-use support::{decl_storage, decl_module, StorageMap, dispatch::Result};
+use support::{
+    decl_storage,
+    decl_module,
+    StorageValue,
+    StorageMap,
+    dispatch::Result,
+    ensure
+};
 use system::ensure_signed;
 use runtime_primitives::traits::{As, Hash};
 use parity_codec_derive::{Encode, Decode};
+use parity_codec::Encode;
 
 pub trait Trait: balances::Trait {}
 
 #[derive(Encode, Decode, Default, Clone, PartialEq)]
-pub struct KittyStruct<Hash, Balance> {
+pub struct Kitty<Hash, Balance> {
     id: Hash,
     dna: Hash,
     price: Balance,
@@ -15,8 +23,11 @@ pub struct KittyStruct<Hash, Balance> {
 
 decl_storage! {
     trait Store for Module<T: Trait> as KittyStorage {
-        // Declare storage and getter functions here
-        Kitty: map T::AccountId  => KittyStruct<T::Hash, T::Balance>;
+        Kitties get(kitty): map T::Hash => Kitty<T::Hash, T::Balance>;
+        KittyOwner get(owner_of): map T::Hash => Option<T::AccountId>;
+        OwnedKitty get(kitty_of_owner): map T::AccountId => T::Hash;
+        
+        Nonce: u64;
     }
 }
 
@@ -27,14 +38,26 @@ decl_module! {
         fn create_kitty(origin) -> Result {
             let sender = ensure_signed(origin)?;
 
-            let new_kitty = KittyStruct {
-                id: <T as system::Trait>::Hashing::hash_of(&0),
-                dna: <T as system::Trait>::Hashing::hash_of(&0),
+            // Nonce and seed a new random hash
+            let nonce = <Nonce<T>>::get();
+            let random_hash = (<system::Module<T>>::random_seed(), &sender, nonce)
+                .using_encoded(<T as system::Trait>::Hashing::hash);
+
+            ensure!(!<Kitties<T>>::exists(random_hash), "The kitty already exists");
+
+            let new_kitty = Kitty {
+                id: random_hash,
+                dna: random_hash,
                 price: <T::Balance as As<u64>>::sa(0),
                 gen: 0
             };
 
-            <Kitty<T>>::insert(sender, new_kitty);
+            <Kitties<T>>::insert(random_hash, new_kitty);
+            <KittyOwner<T>>::insert(random_hash, &sender);
+            <OwnedKitty<T>>::insert(&sender, random_hash);
+
+            // Update nonce
+            <Nonce<T>>::mutate(|n| *n += 1);
 
             Ok(())
         }
